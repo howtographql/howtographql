9a8a7b91fe490280eac8573ab431591140ce9627
Tim Suchanek <tim.suchanek@gmail.com>
2017-06-27 14:33:54 +0200

    added communnity section

---
+++ src/components/home/Bubble.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import Info from '../Info'
+
+interface Props {
+  avatar: string
+  children?: JSX.Element
+  diameter?: number
+  x?: number
+  y?: number
+  name?: string
+  description?: string
+}
+
+export default function Bubble({
+  avatar,
+  children,
+  diameter = 150,
+  x = 0,
+  y = 0,
+  name,
+  description,
+}: Props) {
+  return (
+    <div
+      className="bubble"
+      style={{ width: diameter, height: diameter, top: y, left: x }}
+    >
+      <style jsx={true}>{`
+        .bubble {
+          @p: .relative, .mr25;
+        }
+        img {
+          @p: .br100;
+          width: 100%;
+          height: auto;
+        }
+        h3 {
+          @p: .tc, .nowrap;
+        }
+        div {
+          @p: .mt10, .tc;
+        }
+      `}</style>
+      <Info customTip={<img src={avatar} />}>
+        <h3>{name}</h3>
+        <div>{description}</div>
+      </Info>
+    </div>
+  )
+}
