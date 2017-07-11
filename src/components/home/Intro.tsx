cf4ad0c05161ff84bd714135f1024a49849c2f78
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-11 13:33:14 +0200

    adjust canvas width

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -19,6 +19,7 @@ class Intro extends React.Component<Props, null> {
       <section className="intro">
         <div className="particles">
           <Particles
+            width={`${window && window.innerWidth || 1000}`}
             params={
               {
                 particles: {
