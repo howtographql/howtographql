73f2d0973ccf4d137a89594ccb4600a499e0376a
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-11 13:06:56 +0200

    added graphq

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -4,6 +4,7 @@ import { Step } from '../../types'
 import Link from 'gatsby-link'
 import { connect } from 'react-redux'
 import { setOverviewVideoVisible } from '../../actions/ui'
+import Particles from 'react-particles-js'
 
 interface Props {
   steps: { [key: string]: Step[] }
@@ -15,7 +16,53 @@ class Intro extends React.Component<Props, null> {
   render() {
     return (
       <section className="intro">
+        <div className="particles">
+          <Particles
+            params={
+              {
+                particles: {
+                  number: {value: 60, density: {enable: !0, value_area: 800}},
+                  color: {value: "#fbc6e5"},
+                  shape: {
+                    type: "circle",
+                    stroke: {width: 4, color: "#fbc6e5"},
+                    polygon: {nb_sides: 5}
+                  },
+                  opacity: {value: .2, random: !1, anim: {enable: !1, speed: 1, opacity_min: .6, sync: !1}},
+                  size: {value: 1, random: !0, anim: {enable: !1, speed: 40, size_min: .1, sync: !1}},
+                  line_linked: {enable: !0, distance: 150, color: "#fbc6e5", opacity: .15, width: 1},
+                  move: {
+                    enable: !0,
+                    speed: 1,
+                    direction: "none",
+                    random: !1,
+                    straight: !1,
+                    out_mode: "out",
+                    attract: {enable: !1, rotateX: 600, rotateY: 1200}
+                  }
+                },
+                interactivity: {
+                  detect_on: "canvas",
+                  events: {onhover: {enable: !1, mode: "repulse"}, onclick: {enable: !0, mode: "push"}, resize: !0},
+                  modes: {
+                    grab: {distance: 400, line_linked: {opacity: 1}},
+                    bubble: {distance: 400, size: 40, duration: 2, opacity: 8, speed: 3},
+                    repulse: {distance: 200},
+                    push: {particles_nb: 4},
+                    remove: {particles_nb: 2}
+                  }
+                },
+              } as any
+            }
+          />
+        </div>
         <style jsx={true}>{`
+          .rest {
+            @p: .relative, .z2;
+          }
+          section {
+            @p: .relative;
+          }
           h1 {
             @p: .tc;
             font-size: 54px;
@@ -32,6 +79,9 @@ class Intro extends React.Component<Props, null> {
           }
           .center-container {
             @p: .flex, .justifyCenter;
+          }
+          .particles {
+            @p: .absolute, .left0, .top0, .right0, .bottom0, .flex;
           }
           @media (max-width: 500px) {
             .intro {
@@ -55,33 +105,35 @@ class Intro extends React.Component<Props, null> {
             @p: .mt25;
           }
         `}</style>
-        <h1>The Fullstack Tutorial for GraphQL</h1>
-        <p>
-          The free and open-source tutorial for you to learn about GraphQL from
-          zero to production. After a basic
-          introduction, you’ll build a Hackernews clone with Javascript or any
-          other technology of your choice.
-        </p>
-        <div className="center-container">
-          <div
-            className="watch-overview"
-            onClick={this.props.setOverviewVideoVisible.bind(null, true)}
-          >
-            <img src={require('../../assets/icons/play.svg')} />
-            <span>
-              Watch Overview
-            </span>
+        <div className="rest">
+          <h1>The Fullstack Tutorial for GraphQL</h1>
+          <p>
+            The free and open-source tutorial for you to learn about GraphQL from
+            zero to production. After a basic
+            introduction, you’ll build a Hackernews clone with Javascript or any
+            other technology of your choice.
+          </p>
+          <div className="center-container">
+            <div
+              className="watch-overview"
+              onClick={this.props.setOverviewVideoVisible.bind(null, true)}
+            >
+              <img src={require('../../assets/icons/play.svg')}/>
+              <span>
+                Watch Overview
+              </span>
+            </div>
           </div>
+          <div className="center-container btn-container">
+            <Link to="/basics/0-introduction/">
+              <div className="btn">Start with Introduction</div>
+            </Link>
+          </div>
+          <IntroSteps steps={this.props.steps} location={this.props.location}/>
         </div>
-        <div className="center-container btn-container">
-          <Link to="/basics/0-introduction/">
-            <div className="btn">Start with Introduction</div>
-          </Link>
-        </div>
-        <IntroSteps steps={this.props.steps} location={this.props.location} />
       </section>
     )
   }
 }
 
-export default connect(null, { setOverviewVideoVisible })(Intro)
+export default connect(null, {setOverviewVideoVisible})(Intro)
