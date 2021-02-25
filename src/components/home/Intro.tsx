9aa844d7e554c54994e5fe5e4a46e23028481155
Ryan Chenkie <ryanchenkie@gmail.com>
2021-02-25 09:50:35 -0500

    add prisma button

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -1,16 +1,17 @@
-import * as React from 'react'
-import IntroSteps from './IntroSteps'
-import { Step } from '../../types'
-import Link from 'gatsby-link'
-import { connect } from 'react-redux'
-import { setOverviewVideoVisible } from '../../actions/ui'
-import Particles from 'react-particles-js'
-import Icon from 'graphcool-styles/dist/components/Icon/Icon'
+import * as React from 'react';
+import IntroSteps from './IntroSteps';
+import { Step } from '../../types';
+import Link from 'gatsby-link';
+import { connect } from 'react-redux';
+import { setOverviewVideoVisible } from '../../actions/ui';
+import Particles from 'react-particles-js';
+import Icon from 'graphcool-styles/dist/components/Icon/Icon';
+import PrismaButton from '../PrismaButton';
 
 interface Props {
-  steps: { [key: string]: Step[] }
-  location: any
-  setOverviewVideoVisible: (visible: boolean) => void
+  steps: { [key: string]: Step[] };
+  location: any;
+  setOverviewVideoVisible: (visible: boolean) => void;
 }
 
 class Intro extends React.Component<Props, null> {
@@ -19,43 +20,71 @@ class Intro extends React.Component<Props, null> {
       <section className="intro">
         <div className="particles">
           <Particles
-            style={ { maxWidth: '100%' } }
-            width={`${typeof window !== 'undefined' && window.innerWidth || 1000}`}
+            style={{ maxWidth: '100%' }}
+            width={`${(typeof window !== 'undefined' && window.innerWidth) ||
+              1000}`}
             height="466"
             params={
               {
                 particles: {
-                  number: {value: 20, density: {enable: !0, value_area: 800}},
-                  color: {value: "#ffc0e5"},
-                  shape: {
-                    type: "circle",
-                    stroke: {width: 4, color: "#fdd7ed"},
-                    polygon: {nb_sides: 5}
+                  number: {
+                    value: 20,
+                    density: { enable: !0, value_area: 800 }
                   },
-                  opacity: {value: .2, random: !1, anim: {enable: !1, speed: 1, opacity_min: .6, sync: !1}},
-                  size: {value: 1, random: !0, anim: {enable: !1, speed: 40, size_min: .1, sync: !1}},
-                  line_linked: {enable: !0, distance: 150, color: "#ffc0e5", opacity: .3, width: 1},
+                  color: { value: '#ffc0e5' },
+                  shape: {
+                    type: 'circle',
+                    stroke: { width: 4, color: '#fdd7ed' },
+                    polygon: { nb_sides: 5 }
+                  },
+                  opacity: {
+                    value: 0.2,
+                    random: !1,
+                    anim: { enable: !1, speed: 1, opacity_min: 0.6, sync: !1 }
+                  },
+                  size: {
+                    value: 1,
+                    random: !0,
+                    anim: { enable: !1, speed: 40, size_min: 0.1, sync: !1 }
+                  },
+                  line_linked: {
+                    enable: !0,
+                    distance: 150,
+                    color: '#ffc0e5',
+                    opacity: 0.3,
+                    width: 1
+                  },
                   move: {
                     enable: !0,
                     speed: 1,
-                    direction: "none",
+                    direction: 'none',
                     random: !1,
                     straight: !1,
-                    out_mode: "out",
-                    attract: {enable: !1, rotateX: 600, rotateY: 1200}
+                    out_mode: 'out',
+                    attract: { enable: !1, rotateX: 600, rotateY: 1200 }
                   }
                 },
                 interactivity: {
-                  detect_on: "canvas",
-                  events: {onhover: {enable: !1, mode: "repulse"}, onclick: {enable: !0, mode: "push"}, resize: !0},
+                  detect_on: 'canvas',
+                  events: {
+                    onhover: { enable: !1, mode: 'repulse' },
+                    onclick: { enable: !0, mode: 'push' },
+                    resize: !0
+                  },
                   modes: {
-                    grab: {distance: 400, line_linked: {opacity: 1}},
-                    bubble: {distance: 400, size: 40, duration: 2, opacity: 8, speed: 3},
-                    repulse: {distance: 200},
-                    push: {particles_nb: 4},
-                    remove: {particles_nb: 2}
+                    grab: { distance: 400, line_linked: { opacity: 1 } },
+                    bubble: {
+                      distance: 400,
+                      size: 40,
+                      duration: 2,
+                      opacity: 8,
+                      speed: 3
+                    },
+                    repulse: { distance: 200 },
+                    push: { particles_nb: 4 },
+                    remove: { particles_nb: 2 }
                   }
-                },
+                }
               } as any
             }
           />
@@ -77,16 +106,16 @@ class Intro extends React.Component<Props, null> {
           }
           .watch-overview {
             @p: .black40, .f16, .fw6, .mt25, .pa16, .ttu, .flex, .itemsCenter, .pointer;
-            transition: color .15s ease-in-out;
+            transition: color 0.15s ease-in-out;
           }
           .watch-overview:hover {
             @p: .black60;
           }
           .watch-overview img {
-            transition: filter .15s ease-in-out;
+            transition: filter 0.15s ease-in-out;
           }
           .watch-overview :global(svg) > :global(g) {
-            transition: opacity .15s ease-in-out;
+            transition: opacity 0.15s ease-in-out;
           }
           .watch-overview:hover :global(svg) > :global(g) {
             opacity: 0.6;
@@ -125,18 +154,23 @@ class Intro extends React.Component<Props, null> {
         <div className="rest">
           <h1>The Fullstack Tutorial for GraphQL</h1>
           <p>
-            The free and open-source tutorial to learn all around GraphQL to go from
-            zero to production.
+            The free and open-source tutorial to learn all around GraphQL to go
+            from zero to production.
           </p>
+          <p>Brought to you by the GraphQL Community and</p>
+          <PrismaButton />
           <div className="center-container">
             <div
               className="watch-overview"
               onClick={this.props.setOverviewVideoVisible.bind(null, true)}
             >
-              <Icon src={require('../../assets/icons/play.svg')} color={'black'} width={37} height={37} />
-              <span>
-                Watch Overview
-              </span>
+              <Icon
+                src={require('../../assets/icons/play.svg')}
+                color={'black'}
+                width={37}
+                height={37}
+              />
+              <span>Watch Overview</span>
             </div>
           </div>
           <div className="center-container btn-container">
@@ -144,11 +178,11 @@ class Intro extends React.Component<Props, null> {
               <div className="btn">Start with Introduction</div>
             </Link>
           </div>
-          <IntroSteps steps={this.props.steps} location={this.props.location}/>
+          <IntroSteps steps={this.props.steps} location={this.props.location} />
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default connect(null, {setOverviewVideoVisible})(Intro)
+export default connect(null, { setOverviewVideoVisible })(Intro);
