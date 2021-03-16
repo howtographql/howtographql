352aa650afe313fed6168be960f42a3e3d8cd057
Ryan Chenkie <ryanchenkie@gmail.com>
2021-03-16 15:01:55 -0400

    log props

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === `production`) {
 
 module.exports = class Html extends React.Component {
   render() {
+    console.log(this.props)
     let css
     if (process.env.NODE_ENV === `production`) {
       css = (
